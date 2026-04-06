5ab4d812ad3b8861890e302f5ad738d74d341d51
VincentR3 <vincent03@gmx.de>
2026-04-06 16:07:31 +0200

    fixed errors

--- src/main/scala/chess/util/parser/FastPgnParser.scala
+++ src/main/scala/chess/util/parser/FastPgnParser.scala
@@ -7,36 +7,37 @@ import fastparse.*, NoWhitespace.*
 
 /**
  * A high-performance PGN parser using Fastparse.
+ * Updated for Scala 3 / Fastparse 3 syntax.
  */
 object FastPgnParser extends PgnParser:
 
   // -- Whitespace Handling --
-  def ws[_: P] = P(CharsWhileIn(" \t\r\n", 0))
-  def ws1[_: P] = P(CharsWhileIn(" \t\r\n", 1))
+  def ws(using ctx: P[?]) = P(CharsWhileIn(" \t\r\n", 0))
+  def ws1(using ctx: P[?]) = P(CharsWhileIn(" \t\r\n", 1))
 
   // -- Grammar Components --
-  def tag[_: P] = P("[" ~ CharsWhile(c => c != ']') ~ "]")
-  def tags[_: P] = P(tag.rep(sep = ws))
+  def tag(using ctx: P[?]) = P("[" ~ CharsWhile(c => c != ']') ~ "]").!
+  def tags(using ctx: P[?]) = P(tag.rep(sep = ws))
   
-  def moveNum[_: P] = P(CharsWhileIn("0-9").rep(1) ~ "." ~ " ".? ~ ".".rep(0))
-  def comment[_: P] = P("{" ~ CharsWhile(c => c != '}') ~ "}")
+  def moveNum(using ctx: P[?]) = P(CharsWhileIn("0-9").rep(1) ~ "." ~ " ".? ~ ".".rep(0)).!
+  def comment(using ctx: P[?]) = P("{" ~ CharsWhile(c => c != '}') ~ "}").!
   
-  def sanChar[_: P] = P(CharIn("a-h", "1-8", "NBRQK", "x", "=", "+", "#", "-"))
-  def san[_: P] = P("O-O-O" | "O-O" | sanChar.rep(1)!)
+  def sanChar(using ctx: P[?]) = P(CharIn("a-h1-8NBRQKx=+#\\-"))
+  def san(using ctx: P[?]) = P(("O-O-O" | "O-O" | sanChar.rep(1)).!)
   
-  def result[_: P] = P("1-0" | "0-1" | "1/2-1/2" | "*")
+  def result(using ctx: P[?]) = P(("1-0" | "0-1" | "1/2-1/2" | "*").!)
   
-  def pgn[_: P]: P[List[String]] = P(
+  def pgn(using ctx: P[?]): P[List[String]] = P(
     ws ~ tags.? ~ (ws ~ (moveNum | comment | san | result)).rep ~ ws ~ End
-  ).map { tokens =>
-    tokens.flatten.collect {
-      case s if !s.endsWith(".") && !s.startsWith("{") && !s.contains("-") && s != "*" => s
-      case s if s == "O-O" || s == "O-O-O" => s
+  ).map { case (_, moves) =>
+    moves.collect {
+      case s: String if !s.contains(".") && !s.startsWith("{") && !s.contains("-") && s != "*" => s
+      case s: String if s == "O-O" || s == "O-O-O" => s
     }.toList
   }
 
   def parse(pgnString: String): Try[GameState] = 
-    fastparse.parse(pgnString, pgn(_)) match
+    fastparse.parse(pgnString, pgn(using _)) match
       case Parsed.Success(tokens, _) => 
         Try {
           var state = GameState.initial
